1af468edc4aa110eb46c620f7e143d355b9660c1
James <jamescockfield10@gmail.com>
2022-04-18 18:14:19 +0100

    health and death

--- src/Game.java
+++ src/Game.java
@@ -32,34 +32,39 @@ public class Game extends GameCore
     static int screenHeight = 384;
     int xo = 0;
     int yo = 16;
-
     float gravity = 0.001f;
+    int playerHealth;
     
     // Game state flags
     boolean paused;
     boolean moveLeft, moveRight, still;
     boolean slimeMoveLeft, slimeMoveRight, slimeStill;
+    boolean dead;
 
     // Game Resources
     // Player Animations
-    Animation standingAnim, runningAnim, jumpingAnim, fallingAnim;
+    Animation standingAnim, runningAnim, jumpingAnim, fallingAnim, deadAnim;
     // Slime Animations
     Animation moveAnim;
     // Item Animations
     Animation chestAnim, coinAnim, flagAnim, keyAnim, runeAnim;
     // Background Animations
     Animation background1, background2, background3, background4, background5;
+    // Lives Animations
+    Animation heart1Anim, heart2Anim, heart3Anim;
 
     // Game Entities
     Player player;
     Slime slime;
     Coin coin;
     Flag flag;
+    Heart heart1, heart2, heart3;
     Sprite bg1, bg2, bg3, bg4, bg5; // backgrounds
 
     ArrayList<Sprite> backgrounds = new ArrayList<Sprite>();
     ArrayList<Slime> slimes = new ArrayList<Slime>();
     ArrayList<Coin> coins = new ArrayList<Coin>();
+    ArrayList<Heart> hearts = new ArrayList<Heart>();
 
     TileMap tmap = new TileMap(); // Our tile map, note that we load it in init()
     
@@ -91,11 +96,13 @@ public class Game extends GameCore
         slimeMoveRight = false;
         slimeMoveLeft = true;
         slimeStill = true;
+        dead = false;
+        playerHealth = 3;
 
         // Load the tile map and print it out so we can check it is valid
         tmap.loadMap("maps", "map.txt");
         
-        setSize(tmap.getPixelWidth() / 8, (tmap.getPixelHeight() + 16));
+        setSize(tmap.getPixelWidth() / 6, (tmap.getPixelHeight() + 16));
         setVisible(true);
 
         // Player Animations
@@ -111,6 +118,9 @@ public class Game extends GameCore
 
             fallingAnim = new Animation();
             fallingAnim.loadAnimationFromSheet("images/character/land.png", 2, 1, 90);
+
+            deadAnim = new Animation();
+            deadAnim.loadAnimationFromSheet("images/character/dead.png", 7, 1, 90);
         }
 
         // Slime Animations
@@ -136,6 +146,15 @@ public class Game extends GameCore
 
             runeAnim = new Animation();
             runeAnim.loadAnimationFromSheet("images/items/Rune.png", 4, 1, 125);
+
+            heart1Anim = new Animation();
+            heart1Anim.loadAnimationFromSheet("images/items/heart.png", 4, 1, 250);
+
+            heart2Anim = new Animation();
+            heart2Anim.loadAnimationFromSheet("images/items/heart.png", 4, 1, 250);
+
+            heart3Anim = new Animation();
+            heart3Anim.loadAnimationFromSheet("images/items/heart.png", 4, 1, 250);
         }
 
         // Background Animations
@@ -231,17 +250,29 @@ public class Game extends GameCore
             }
         }
 
-        // Initialise backgrounds
+        // Lives Setup
         {
+            // Initialise Hearts
+            heart1 = new Heart(heart1Anim);
+            heart2 = new Heart(heart2Anim);
+            heart3 = new Heart(heart3Anim);
+
+            // Add hearts to arraylist
+            hearts.add(heart1);
+            hearts.add(heart2);
+            hearts.add(heart3);
+        }
+
+        // Background Setup
+        {
+            // Initialise backgrounds
             bg1 = new Sprite(background1);
             bg2 = new Sprite(background2);
             bg3 = new Sprite(background3);
             bg4 = new Sprite(background4);
             bg5 = new Sprite(background5);
-        }
 
-        // Add backgrounds to an arraylist
-        {
+            // Add backgrounds to an arraylist
             backgrounds.add(bg1);
             backgrounds.add(bg2);
             backgrounds.add(bg3);
@@ -295,13 +326,30 @@ public class Game extends GameCore
         player.setOffsets(xo, yo);
         player.drawTransformed(g);
 
+        int i = 1;
+        for (Heart h: hearts) {
+            h.setPosition((37 * i), 52);
+            h.draw(g);
+            i++;
+        }
+
         // Apply offsets to tile map and draw  it
         tmap.draw(g, xo, yo);
-        
+
         // Show score and status information
         String msg = String.format("Score: %d", (score/100) + (totalCoinsCollected * 10));
         g.setColor(Color.white);
         g.drawString(msg, getWidth() - 80, 65);
+
+        if (dead) {
+            FontMetrics metrics = g.getFontMetrics(getFont()); // Allows you to get the x and y length of a string to allow the string to be in the middle of the screen
+            String msgDead1 = String.format("You died with a Score of: %d", (score/100) + (totalCoinsCollected * 10));
+            String msgDead2 = "Press ESC to quit";
+            g.setColor(Color.white);
+            g.drawString(msgDead1, ((getWidth()/2) - (metrics.stringWidth(msgDead1)/2)), getHeight()/2);
+            g.drawString(msgDead2, ((getWidth()/2) - (metrics.stringWidth(msgDead2)/2)), ((getHeight()/2) + metrics.getHeight() + 5));
+            paused = true;
+        }
     }
 
     /**
@@ -334,17 +382,21 @@ public class Game extends GameCore
             c.update(elapsed);
         }
 
+        for (Heart h: hearts) {
+            h.update(elapsed);
+        }
+
         if (player.getX() <= bg1.getWidth()) {
             xo = 0;
         }
 
         //  && player.getX() <= (tmap.getPixelWidth() - bg1.getWidth())
 
-        if (player.getX() >= ((tmap.getPixelWidth() / 8) / 2) && player.getX() <= 3304) {
+        if (player.getX() >= ((float) (tmap.getPixelWidth() / 8) / 2) && player.getX() <= 3304) {
             xo = (int) (((tmap.getPixelWidth() / 8) / 2) - player.getX());
         }
 
-        if (player.getX() <= ((tmap.getPixelWidth() / 8) / 2) || player.getX() >= 3304) {
+        if (player.getX() <= ((float) (tmap.getPixelWidth() / 8) / 2) || player.getX() >= 3304) {
             for (Sprite bg: backgrounds) {
                 bg.setVelocityX(0);
             }
@@ -395,22 +447,51 @@ public class Game extends GameCore
         else if (!player.isOnGround() && player.getVelocityY() > 0)
             player.setAnimation(fallingAnim);
 
-        // Then check for any collisions that may have occurred
-        handleScreenEdge(player, tmap, elapsed);
-        handleScreenEdge(slime, tmap, elapsed);
-        player.checkTileCollision(player, tmap);
         for (Slime s: slimes) {
             s.checkTileCollision(s, tmap);
         }
 
         coins.removeIf(c -> {
-            if (boundingBoxCollision(player, c)){
+            if (boundingBoxCollision(player, c)) {
                 totalCoinsCollected++;
                 return true;
             } else {
                 return false;
             }
         });
+
+        slimes.removeIf(s -> {
+            if (boundingBoxCollision(player, s)) {
+                playerHealth--;
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        switch (playerHealth) {
+            case 0: {
+                hearts.get(0).getAnimation().pauseAt(3);
+                player.setAnimation(deadAnim);
+                if ((hearts.get(0).getImage() == hearts.get(0).getAnimation().getFrameImage(3)) && (player.getImage() == player.getAnimation().getFrameImage(6))) {
+                    dead = true;
+                }
+                break;
+            }
+            case 1: {
+                hearts.get(1).getAnimation().pauseAt(3);
+                break;
+            }
+            case 2: {
+                hearts.get(2).getAnimation().pauseAt(3);
+                break;
+            }
+        }
+
+        // Then check for any collisions that may have occurred
+        handleScreenEdge(player, tmap, elapsed);
+        handleScreenEdge(slime, tmap, elapsed);
+        player.checkTileCollision(player, tmap);
     }
 
     /**
@@ -424,8 +505,7 @@ public class Game extends GameCore
     	// This method just checks if the sprite has gone off the bottom screen.
     	// Ideally you should use tile collision instead of this approach
     	
-        if (s.getY() + s.getHeight() > tmap.getPixelHeight())
-        {
+        if (s.getY() + s.getHeight() > tmap.getPixelHeight()) {
         	// Put the player back on the map 1 pixel above the bottom
         	s.setY(tmap.getPixelHeight() - s.getHeight());
         }
@@ -441,11 +521,24 @@ public class Game extends GameCore
     	int key = e.getKeyCode();
 
         switch (key) {
-            case KeyEvent.VK_ESCAPE: stop(); break;
-            case KeyEvent.VK_LEFT: moveLeft = true; break;
-            case KeyEvent.VK_RIGHT: moveRight = true; break;
+            case KeyEvent.VK_ESCAPE: {
+                stop();
+                break;
+            }
+            case KeyEvent.VK_LEFT: {
+                if (!dead) {
+                    moveLeft = true;
+                }
+                break;
+            }
+            case KeyEvent.VK_RIGHT: {
+                if (!dead) {
+                    moveRight = true;
+                }
+                break;
+            }
             case KeyEvent.VK_SPACE: {
-                if (player.isOnGround()) {
+                if (player.isOnGround() && !dead) {
                     player.setVelocityY(-0.4f);
                     player.setIsOnGround(false);
                 }
@@ -459,10 +552,7 @@ public class Game extends GameCore
         Rectangle sprite1 = new Rectangle((int) s1.getX(), (int) s1.getY(), s1.getWidth(), s1.getHeight());
         Rectangle sprite2 = new Rectangle((int) s2.getX(), (int) s2.getY(), s2.getWidth(), s2.getHeight());
 
-        if (sprite1.intersects(sprite2))
-            return true;
-        else
-            return false;
+        return sprite1.intersects(sprite2);
     }
 
     public void keyReleased(KeyEvent e) {
@@ -473,11 +563,33 @@ public class Game extends GameCore
 		// Need to use break to prevent fall through.
 		switch (key)
 		{
-			case KeyEvent.VK_ESCAPE : stop(); break;
-			case KeyEvent.VK_P : paused = !paused; break;
-            case KeyEvent.VK_R: player.setPosition(tmap.getTileWidth() + player.getWidth(), tmap.getPixelHeight() - 128); xo = 0; break;
-            case KeyEvent.VK_LEFT: moveLeft = false; player.stopMoving(); break;
-            case KeyEvent.VK_RIGHT: moveRight = false; player.stopMoving(); break;
+			case KeyEvent.VK_P : {
+                if (!dead) {
+                    paused = !paused;
+                }
+                break;
+            }
+            case KeyEvent.VK_R: {
+                if (!dead) {
+                    player.setPosition(tmap.getTileWidth() + player.getWidth(), tmap.getPixelHeight() - 128);
+                    xo = 0;
+                }
+                break;
+            }
+            case KeyEvent.VK_LEFT: {
+                if (!dead) {
+                    moveLeft = false;
+                    player.stopMoving();
+                }
+                break;
+            }
+            case KeyEvent.VK_RIGHT: {
+                if (!dead) {
+                    moveRight = false;
+                    player.stopMoving();
+                }
+                break;
+            }
             default :  break;
 		}
 	}
---
+++ src/game2D/entities/Items/Heart.java
@@ -0,0 +1,15 @@
+package game2D.entities.Items;
+
+import game2D.Animation;
+import game2D.Sprite;
+
+public class Heart extends Sprite {
+    /**
+     * Creates a new Sprite object with the specified Animation.
+     *
+     * @param anim The animation to use for the sprite.
+     */
+    public Heart(Animation anim) {
+        super(anim);
+    }
+}
